4e53339c98439bdfaff2fc1753b524c59cd5b1c2
Cheong JaeYoon <jyjun9910@ajou.ac.kr>
2022-05-25 15:03:53 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <router-view/>
+</template>
+
+<style>
+@font-face {
+    font-family: 'SuseongDotum';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2205@1.0/SuseongDotum.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+}
+#app {
+  font-family: 'SuseongDotum', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: black;
+  background-color: #eeeeee;
+  width: 100%;
+  height: 100%;
+}
+input{
+  font-family: 'SuseongDotum', Helvetica, Arial, sans-serif;
+}
+html, body{
+  margin: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home">
+    <topBar/>
+    <div v-if="this.$store.state.CurrentPage == 'Board'">
+      <Board/>
+    </div>
+    <div v-if="this.$store.state.CurrentPage == 'Write'">
+      <Write/>
+    </div>
+    <div v-if="this.$store.state.CurrentPage == 'Content'">
+      <Content/>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TopBar from '@/components/TopBar.vue'
+import Board from '@/components/Board.vue'
+import Write from '@/components/Write.vue'
+import Content from '@/components/Content.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    TopBar,
+    Board,
+    Write,
+    Content
+  }
+}
+</script>
+<style scoped>
+.home{
+  background-color: white;
+  width: 1300px;
+  height: 100%;
+  margin: 0 auto;
+  overflow: scroll;
+}
+</style>
